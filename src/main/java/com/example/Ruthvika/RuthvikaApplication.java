627e9c8aee4d746dffc578d65ac8aa5ac860dc40
Nagendra Tokala <nagendratokala@Nagendras-MacBook-Air.local>
2023-02-20 17:22:28 +0530

    First commit

---
+++ src/main/java/com/example/Ruthvika/RuthvikaApplication.java
@@ -0,0 +1,14 @@
+package com.example.Ruthvika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuthvikaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuthvikaApplication.class, args);
+		System.out.println("jjkhkhjkh");
+	}
+
+}
---
+++ src/test/java/com/example/Ruthvika/RuthvikaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Ruthvika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuthvikaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
